830f5f6e16cd5092910816dac5236b3c3f58f9b5
Joseph Bourne <josephpbourne@gmail.com>
2019-05-24 14:08:22 -0700

    added image asset, and the css files from evaload_template, and only the header portion of the index.html page from the evaload_template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Nav.css';
+import './Header.css';
+import './Reset.css';
+import './index.css';
+import './Section.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="index-header">
+      <nav class="fluid-nav">
+          <ul class="ul-wrapper">
+              <li class="nav-links"><a href="whyus.html">Why Us?</a></li>
+              <li class="nav-links"><a href="whatwedo.html">Services</a></li>
+              <li class="nav-links"><a href="index.html">evaload</a></li>
+              <li class="nav-links"><a href="#">444-444-4444</a></li>
+              <li class="nav-links"><a href="login.html">Login</a></li>
+              <li class="nav-links"><a href="signup.html">Get Started</a></li>
+          </ul>
+      </nav>
+      <div class="hd-idx-wrapper">
+          <div class="hd-msg">
+              <h1>Work. Life. Synergy</h1>
+              <h3>US-based Virtual Assistants for entrepreneurs,
+                  professionals, and startups.
+              </h3>
+          </div>
+          <div class="hd-btn-wrapper">
+              <div class="btn-holder">
+                  <button class="btn-consult">
+                      <a href="contact.html">
+                          Request a Consultation
+                      </a>
+                  </button>
+              </div>
+              <div class="btn-holder">
+                  <button class="btn-pricing">
+                      <a href="#">
+                          View Pricing
+                      </a>
+                  </button>
+              </div>
+          </div>
+      </div>
+  </div>
   );
 }
 
