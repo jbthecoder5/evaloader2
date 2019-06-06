11bf923e05727edc38db608032a7d9742104d663
Joseph Bourne <josephpbourne@gmail.com>
2019-06-06 13:36:50 -0700

    deleted the react router code

--- src/App.js
+++ src/App.js
@@ -7,43 +7,22 @@ import './Reset.css';
 import './index.css';
 import './Section.css';
 
-function Index() {
-    return (
-        <div>this is index</div>
-    );
-}
-
-function About() {
-    return (
-        <div>this is the about page</div>
-    );
-}
-
-function Services() {
-    return (
-        <div>this is the service page</div>
-    );
-}
 
 function App() {
     return (
     <div class="index-header">
-            <Router>
                 <nav class="fluid-nav">
                 <ul class="ul-wrapper">
-                    <li class="nav-links"><Link to="/about">Why Us?</Link></li>
-                    <li class="nav-links"><Link to="/services">Services</Link></li>
-                    <li class="nav-links"><Link to="/">evaload</Link></li>
+                    <li class="nav-links"><a href="#">Why Us?</a></li>
+                    <li class="nav-links"><a href="#">Service</a></li>
+                    <li class="nav-links"><a href="#">evaload</a></li>
                     <li class="nav-links"><a href="#">444-444-4444</a></li>
                     <li class="nav-links"><a href="login.html">Login</a></li>
                     <li class="nav-links"><a href="signup.html">Get Started</a></li>
                 </ul>
                 </nav>
 
-                <Route path="/" component={Index} />
-                <Route path="/about" component={About} />
-                <Route path="/services" component={Services} />
-            </Router>
+                
             <div class="hd-idx-wrapper">
                 <div class="hd-msg">
                     <h1>Work. Life. Synergy</h1>
